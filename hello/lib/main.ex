6dfda506cdf3ece60baeeed39262198b21dc233b
devtunis <nahdigyth@gmail.com>
2026-08-16 14:57:46 +0100

    usekeywordkey

--- hello/lib/main.ex
+++ hello/lib/main.ex
@@ -2,6 +2,13 @@ defmodule Main do
   def go do
    Sayhi.greet("ahmed",admin: true)
    Sayhi.from([{:where,true}])
+   Sayhi.introduce(username: "ghaith",lastname: "nahdi", job: "software enginenr",
+   active: true ,
+   isadmin: false
+   )
+
+  Sayhi.usekey([{:name,"ghaith"}])
+  Sayhi.usekeywordkey([name: "ghaith nahdi"])
 
 
   end
--- hello/lib/sayhi.ex
+++ hello/lib/sayhi.ex
@@ -1,4 +1,6 @@
 defmodule Sayhi do
+
+
   def greet(name,option) do
     if(option[:admin]) do
 
@@ -8,9 +10,33 @@ defmodule Sayhi do
     IO.puts("this not a software devs")
 
   end
-end
+  end
 
 def from(option) do
   IO.inspect(option[:where])
+  end
+def introduce(option) do
+  username  = option[:username]
+  lastname =  option[:lastname]
+  job       = option[:job]
+  active  = option[:active]
+  isadmin = option[:isadmin]
+
+  IO.puts("welcome #{username} and lastname is
+  #{lastname} and your job #{job} and you current
+  #{active} and current is admin  #{isadmin}
+  ")
 end
+
+def usekey(option) do
+
+  IO.puts("welcome user #{option[:name]}")
+
+ end
+
+def usekeywordkey(options) do
+  name = Keyword.get(options, :name)
+  IO.puts(name)
+end
+
 end
