98517bfa11fc831e915e7f40108aaeb41f6b1dd8
devtunis <nahdigyth@gmail.com>
2026-08-17 15:45:09 +0100

    case

--- hello/lib/games.ex
+++ hello/lib/games.ex
@@ -7,18 +7,26 @@ defmodule Games do
       price: 4.99
       }
   end
-  def validate_purchase(game)  do
+
+   def purchase_game(game) do
+
+     case  validate_purchase(game) do
+           {:error,res} -> "Error: #{res}"
+           {:ok,_} -> "Successful pruchase"
+     end
+ end
+
+
+
+  defp validate_purchase(game)  do
 
     cond do
       !game.in_stock ->  {:error,"Game not availabe for purchase !"}
        game.price>50 ->   {:error  , "To Expensive! Can't buy"}
-       true ->   {:ok , purchase_game(game) }
+       true ->   {:ok , "Proceed with the Purchase!"}
     end
 
  end
 
- def purchase_game(game) do
-  text = "the name of the game you wanna get it is  #{game.name} with price #{game.price}"
 
- end
 end
