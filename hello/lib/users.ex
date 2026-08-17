dc8a355ce05a652ccbcee3e4a73f9741349c82e4
devtunis <nahdigyth@gmail.com>
2026-08-17 15:14:15 +0100

    closure

--- hello/lib/users.ex
+++ hello/lib/users.ex
@@ -5,20 +5,24 @@ defmodule Users do
 
 
   #new/2
-  def new("Daniel",age)  do
+  def new("Daniel",age)   do
 
     %{name: "Daniel"  ,age: age ,can_drink: true ,is_cool: true}
   end
   #new/2
-  def new(name,age) when age>=18  do
+  def new(name,age) when age>=18 and is_number(age)   do
     formatedd_name = trim(name)
     %{name: formatedd_name  ,age: age ,can_drink: true ,is_cool: true}
   end
 
 
-    def new(name,age)    do
+  def new(name,age) when  is_number(age)   do
     formatedd_name = trim(name)
     %{name: formatedd_name  ,age: age ,can_drink: false ,is_cool: true}
+  end
+
+  def new(_,_) do
+    IO.puts("INVALID AGE")
   end
 
   #new/0
@@ -31,6 +35,9 @@ defmodule Users do
     @baseurl
   end
 
+  def  devidebyzero(value) when value>0 and is_integer(value) do
+    value/10
+  end
 
 
 end
