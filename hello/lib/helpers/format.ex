d6272d9053dadd0e24b73bcc2bfcd564950125be
devtunis <nahdigyth@gmail.com>
2026-08-17 02:29:23 +0100

    Documention

--- hello/lib/helpers/format.ex
+++ hello/lib/helpers/format.ex
@@ -1,4 +1,24 @@
 defmodule Helpers.Format do
 
+@moduledoc """
+
+    Documention For `helpers.Format` .
+    """
+    @doc"""
+    Trim text.
+    What ever I write here
+    exmple usage
+    tim("   fooo   ")
+    result -> "foo"
+    ##Exmples
+    iex>create_course(%{filed: value})
+    """
+
+
+
+def trim(text) do
+  String.trim(text)
+
+end
 
 end
--- hello/lib/users.ex
+++ hello/lib/users.ex
@@ -1,14 +1,23 @@
 defmodule Users do
+  import   Helpers.Format,only: [trim: 1]
+
+  @baseurl "http://localhost:3000/user/v1"
+
   def new(name,age) do
-    %{name: name ,age: age}
+    formatedd_name = trim(name)
+    %{name: formatedd_name  ,age: age}
   end
 
   def new  , do: genreate_default_user()
 
-
   defp genreate_default_user do
       %{name: "Default John doe",age: 30}
   end
+  def base() do
+    @baseurl
+  end
+
+
 end
 
 Users.new()
